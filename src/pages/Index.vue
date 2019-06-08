c99114e098fc235286a598e98a979a2f34e542f3
Thibault Malfoy <thibault.malfoy@gmail.com>
2019-06-08 15:34:32 +0200

    Query allPost

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,32 @@
 <template>
   <Layout>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135"/>
-
-    <h1>Hello, world!</h1>
+    <h1>Bienvenue</h1>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+    <div v-for="edge in $page.posts.edges" :key="edge.node.id">
+      <h2>{{ edge.node.title }}</h2>
+    </div>
   </Layout>
 </template>
+
+<page-query>
+query Posts {
+  posts: allPost(sortBy: "date") {
+    edges {
+      node { 
+        id
+        title
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
   metaInfo: {
-    title: "Hello, world!"
+    title: "Bienvenue"
   }
 };
 </script>
-
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
-</style>
