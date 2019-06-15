efb36c72173a8148311a8fabcfa5a98fe85d9030
Thibault Malfoy <thibault.malfoy@gmail.com>
2019-06-15 17:37:47 +0200

    Create a 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <layout>
+    <h1>Oups, une erreur s’est produite</h1>
+    <p>La page que vous recherchez n’existe pas ou n’est plus disponible à cette adresse. Veuillez retourner à l’accueil pour vous orienter. Merci.</p>
+  </layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Erreur 404"
+  }
+};
+</script>
